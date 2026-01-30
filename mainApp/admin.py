962387900dca16497fba463e0891f2c552551d35
ShukurulloMuminov <shukurullomuminov.info@gmail.com>
2026-01-30 20:25:19 +0500

    Kutubxona_admin+jazzmin

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,42 +1,52 @@
 from django.contrib import admin
-from .models import Talaba, Muallif, Kitob, Kutubxonachi, Record
+from .models import *
 
-
-@admin.register(Talaba)
 class TalabaAdmin(admin.ModelAdmin):
     list_display = ('id', 'ism', 'guruh', 'kurs', 'kitob_soni')
-    search_fields = ('ism', 'guruh')
-    list_filter = ('kurs',)
+    list_display_links = ('id', 'ism')
+    list_filter = ('guruh', 'kurs')
+    list_editable = ('kurs', 'kitob_soni')
+
+    search_help_text = "Ism bo'yicha qidiring!"
+    search_fields = ('ism',)
 
 
-@admin.register(Muallif)
+
+class KitobInline(admin.StackedInline):
+    model = Kitob
+    extra = 1
+
+
 class MuallifAdmin(admin.ModelAdmin):
     list_display = ('id', 'ism', 'jins', 't_sana', 'kitob_soni', 'tirik')
-    search_fields = ('ism',)
-    list_filter = ('jins', 'tirik')
+    list_display_links = ('id', 'ism')
+    list_editable = ('kitob_soni', 'tirik',)
+    inlines = (KitobInline,)
 
-
-@admin.register(Kitob)
-class KitobAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nom', 'janr', 'sahifa', 'muallif')
-    search_fields = ('nom',)
-    list_filter = ('janr',)
-
-
-@admin.register(Kutubxonachi)
-class KutubxonachiAdmin(admin.ModelAdmin):
-    list_display = ('id', 'ism', 'ish_vaqti')
     search_fields = ('ism',)
 
+    list_filter = ( 'tirik',)
 
-@admin.register(Record)
 class RecordAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'talaba',
-        'kitob',
-        'kutubxonachi',
-        'olingan_sana',
-        'qaytargan_sana'
-    )
-    list_filter = ('olingan_sana',)
+    list_display = ('talaba', 'olingan_sana', 'qaytargan_sana', 'kutubxonachi', 'kitob')
+    search_fields = ('talaba__ism', 'kutubxonachi__ism', 'kitob__nom')
+    search_help_text = "Ism bo'yicha qidiring!"
+    date_hierarchy = 'qaytargan_sana'
+
+
+class KitobAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'janr', 'sahifa', 'muallif')
+    search_fields = ('nom',)
+
+
+class KutubxonachiAdmin(admin.ModelAdmin):
+    list_display = ('ism', 'ish_vaqti')
+    search_fields = ('ism',)
+    list_filter = ('ish_vaqti',)
+
+
+admin.site.register(Kutubxonachi, KutubxonachiAdmin)
+admin.site.register(Kitob, KitobAdmin)
+admin.site.register(Muallif, MuallifAdmin)
+admin.site.register(Talaba, TalabaAdmin)
+admin.site.register(Record, RecordAdmin)
